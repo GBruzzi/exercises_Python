42e6454c5319e0386c5ef589827f0951c5020998
GBruzzi <gbruzzi@sga.pucminas.br>
2023-07-09 13:21:35 -0300

    correct 17

--- ex17.py
+++ ex17.py
@@ -1,7 +1,7 @@
 # Analisador de um grupo
 
 soma = 0
-n = 0
+quantM = 0
 
 idadeHomemVelho = 0
 nomeHomem = ''
@@ -16,9 +16,12 @@ for c in range(0,4) :
     if sexo == 'H' and idade >  idadeHomemVelho :
         idadeHomemVelho = idade
         nomeHomem = nome
+    if sexo == 'M' and idade < 21 :
+        quantM += 1    
     print('--------------------------------')
 
 media = soma / 4
 
 print(f'A média de idade do grupo é {media:.2F}')
 print(f'O homem mais velho se chama {nomeHomem} e tem {idadeHomemVelho} anos')
+print(f'Ao todo , são {quantM} mulheres com menos de 20 anos')
